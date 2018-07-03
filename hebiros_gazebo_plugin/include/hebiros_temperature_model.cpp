5b4955485ec35fb7c744b66ff212c7d9cf46a5c1
Matthew Tesch <matt@hebirobotics.com>
2018-07-03 15:35:48 -0400

    Tabs -> spaces snafu

--- hebiros_gazebo_plugin/include/hebiros_temperature_model.cpp
+++ hebiros_gazebo_plugin/include/hebiros_temperature_model.cpp
@@ -6,12 +6,12 @@ namespace hebiros {
     {
       double r_be = 8.0; // (found experimentally from steady-state experiments)
       return TemperatureModel(
-		    2.57 * 1.8,     // r_wh (datasheet)
-		    1.0,            // r_hb (Open air from data sheet divided by empirical factor)
-		    r_be,              
-		    0.943 / 2.57,   // c_w (winding, datasheet; tc_winding / rth_winding)
-		    390.0 / 23.5,   // c_h (housing, datasheet) (TC / R)
-		    10000.0 / r_be  // c_b actuator body; from experimentally measured time constant
+        2.57 * 1.8,     // r_wh (datasheet)
+        1.0,            // r_hb (Open air from data sheet divided by empirical factor)
+        r_be,              
+        0.943 / 2.57,   // c_w (winding, datasheet; tc_winding / rth_winding)
+        390.0 / 23.5,   // c_h (housing, datasheet) (TC / R)
+        10000.0 / r_be  // c_b actuator body; from experimentally measured time constant
       );
     }
 
@@ -20,11 +20,11 @@ namespace hebiros {
       double r_be = 4.0; // (found experimentally from steady-state experiments)
       return TemperatureModel(
         1.41 * 1.6,     // r_wh (datasheet)
-		    1.0,            // r_hb (Open air from data sheet divided by empirical factor)
-		    r_be,              
-		    0.9 / 1.41,     // c_w (winding, datasheet; tc_winding / rth_winding)
-		    427.0 / 17.7,   // c_h (housing, datasheet) (TC / R)
-		    10000.0 / r_be  // c_b actuator body; from experimentally measured time constant
+        1.0,            // r_hb (Open air from data sheet divided by empirical factor)
+        r_be,              
+        0.9 / 1.41,     // c_w (winding, datasheet; tc_winding / rth_winding)
+        427.0 / 17.7,   // c_h (housing, datasheet) (TC / R)
+        10000.0 / r_be  // c_b actuator body; from experimentally measured time constant
       );
     }
 
