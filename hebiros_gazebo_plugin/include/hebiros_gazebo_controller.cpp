98f326568c52c203002cce8fba9350348d09e8ae
Joseph Coombe <jcoombe@metamorphsoftware.com>
2018-07-06 11:54:47 -0500

    Apply low-pass filter to force output

--- hebiros_gazebo_plugin/include/hebiros_gazebo_controller.cpp
+++ hebiros_gazebo_plugin/include/hebiros_gazebo_controller.cpp
@@ -366,9 +366,9 @@ double HebirosGazeboController::ComputeForce(std::shared_ptr<HebirosGazeboGroup>
   hebiros_joint->temperature.update(power_in, iteration_time.toSec());
   hebiros_joint->temperature_safety.update(hebiros_joint->temperature.getMotorWindingTemperature());
 
-  //alpha = hebiros_joint->low_pass_alpha;
-  //force = (force * alpha) + hebiros_joint->prev_force * (1 - alpha);
-  //hebiros_joint->prev_force = force;
+  alpha = hebiros_joint->low_pass_alpha;
+  force = (force * alpha) + hebiros_joint->prev_force * (1 - alpha);
+  hebiros_joint->prev_force = force;
 
   return force;
 }
