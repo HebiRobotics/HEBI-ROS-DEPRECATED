390221afc247109abab403a14db271a7b48ba602
Hardik Singh <singh.hardik1997@gmail.com>
2018-07-10 19:11:15 -0400

    Clearing the confusion pt1

--- example_nodes/include/hebiros/hebiros_publishers_physical.cpp
+++
@@ -1,9 +0,0 @@
-#include "hebiros_publishers_physical.h"
-
-#include "hebiros.h"
-
-
-void HebirosPublishersPhysical::registerGroupPublishers(std::string group_name) {
-
-  HebirosPublishers::registerGroupPublishers(group_name);
-}
